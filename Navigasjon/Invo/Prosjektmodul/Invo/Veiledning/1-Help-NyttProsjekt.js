10345a6b7f55020d99c83205243526e0e3f656a8
Fredrik Slungård <FredrikSlungard@users.noreply.github.com>
2021-11-04 12:04:15 +0100

    La til hjelpeside

---
+++ Navigasjon/Invo/Prosjektmodul/Invo/Veiledning/1-Help-NyttProsjekt.js
@@ -0,0 +1,18 @@
+// https://asconsolvo.sharepoint.com/sites/project/Lists/InvoSettingsList/AllItems.aspx?OR=Teams%2DHL&CT=1635509871299&sortField=LinkTitle&isAscending=true&viewid=4690fd60%2Da47c%2D4ce6%2Dbb94%2Dc4f2f281046a
+
+var condition = function (params) { return params.ctx._spPageContextInfo.webServerRelativeUrl === params.ctx._spPageContextInfo.siteServerRelativeUrl; }
+var runFunction = function (params) { 
+    alert('Her blir du tatt til en video/app som viser hvordan du oppretter prosjekt'); 
+}
+return {
+    "Title": "Nytt Prosjekt",
+    "Id": "InvoHelpNewProject",
+    Order: 100,
+    "ImageUrl": "https://img.icons8.com/material-outlined/24/000000/help.png",
+    "Type": "Function",
+    "Condition": condition,
+    "RunFunction": runFunction,
+    "GroupTitle": "Veiledning",
+    GroupOrder: 900,
+    "GroupType": "Upper"
+};
