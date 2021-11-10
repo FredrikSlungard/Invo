06eb8ec5e0e8ec99c9ee0877af2226258580864e
Fredrik Slungård <FredrikSlungard@users.noreply.github.com>
2021-11-10 08:49:15 +0100

    la til en test html

--- Navigasjon/Invo/Prosjektmodul/Invo/Veiledning/2-Help-Oppgaver.js
+++ Navigasjon/Invo/Prosjektmodul/Invo/Veiledning/2-Help-Oppgaver.js
@@ -1,9 +1,24 @@
 // https://asconsolvo.sharepoint.com/sites/project/Lists/InvoSettingsList/AllItems.aspx?OR=Teams%2DHL&CT=1635509871299&sortField=LinkTitle&isAscending=true&viewid=4690fd60%2Da47c%2D4ce6%2Dbb94%2Dc4f2f281046a
 
 var condition = function (params) { return params.ctx._spPageContextInfo.webServerRelativeUrl === params.ctx._spPageContextInfo.siteServerRelativeUrl; }
-var runFunction = function (params) { 
-    alert('Her blir du tatt til en video/app som viser hvordan du bruker oppgavene'); 
+var runFunction = function (params) {
+    
+    let userLogInName = _spPageContextInfo.userLoginName;
+    let userProperties = _spPageContextInfo.userProperties;
+    alert(userLogInName);
+
 }
+
+// Funksjon som sjekker om brukeren har riktig tilgang
+var accessFunction = function (params) {
+    let showBtnForGroups = ''; // Liste med navn som skal vise gruppene
+
+    /*
+    hvis bruker tilhører gruppe i showBtnForGroups vis knapp, ellers skjules den
+    Tilsvarer standard SP løsning for brukerrettet navigasjon
+    */
+}
+
 return {
     "Title": "Oppgavestyring",
     "Id": "InvoHelpNewProject",
@@ -14,5 +29,7 @@ return {
     "RunFunction": runFunction,
     "GroupTitle": "Veiledning",
     GroupOrder: 900,
-    "GroupType": "Upper"
+    "GroupType": "Upper",
+    "GroupAcces": accessFunction
 };
+
---
+++ Navigasjon/Invo/skjulNavigasjon.js
@@ -0,0 +1,8 @@
+/*<script src="/_layouts/15/SP.Runtime.js" type="text/javascript"></script>
+<script src="/_layouts/15/SP.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+// if the above function doesn't work try to use the below one
+//SP.SOD.executeFunc('sp.js', 'SP.ClientContext', GetCurrentUserInfo);*/
+
+//ExecuteOrDelayUntilScriptLoaded(GetCurrentUserInfo,'sp.js');
